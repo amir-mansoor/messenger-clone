d6f214d630b32af3dc8b391e7518db8637d044c3
amir-mansoor <qazimansor89@gmail.com>
2024-12-09 20:44:50 +0500

    move scrollbar to bottom on new message

--- frontend/src/screens/ChatScreen.jsx
+++ frontend/src/screens/ChatScreen.jsx
@@ -68,9 +68,13 @@ const ChatScreen = () => {
 
   const { data: chats } = useGetChatsQuery();
 
-  const { data: messagesData, refetch } = useGetMessagesQuery(
+  const {
+    data: messagesData,
+    refetch,
+    isFetching,
+  } = useGetMessagesQuery(
     { chatId },
-    { skip: !chatId, refetchOnMountOrArgChange: true } // Only fetch if chatId exists
+    { skip: !chatId } // Only fetch if chatId exists
   );
 
   useEffect(() => {
@@ -89,6 +93,7 @@ const ChatScreen = () => {
   useEffect(() => {
     socket.on("getMessage", (data) => {
       setArivalMessage(data);
+      scrollToBottom();
     });
   }, []);
 
@@ -100,7 +105,6 @@ const ChatScreen = () => {
         (user) => user._id === arivalMessage.sender
       );
       if (userExists) {
-        console.log(arivalMessage);
         dispatch(appendMessage(arivalMessage));
       }
     }
@@ -219,30 +223,52 @@ const ChatScreen = () => {
             </div>
 
             {/* Scrollable Messages */}
-            <div
-              className="flex-1 overflow-y-auto p-4"
-              style={{ minHeight: 0 }} // Ensures flex behavior works properly
-            >
-              {messages.map((message, index) => (
-                <div
-                  key={index}
-                  className={`flex ${
-                    message.sender === userInfo.id
-                      ? "justify-end"
-                      : "justify-start"
-                  } p-2`}
-                >
-                  <div
-                    className={`bg-gray-600 text-white p-3 rounded-lg max-w-[30%] h-auto break-words ${
-                      message.role === "user" ? "bg-blue-600" : "bg-gray-500"
-                    }`}
+            {isFetching ? (
+              <div className="flex-1 overflow-y-auto p-4">
+                <div role="status" className="flex justify-center mt-10">
+                  <svg
+                    aria-hidden="true"
+                    className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    {message.text}
-                  </div>
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
                 </div>
-              ))}
-              <div ref={messagesEndRef} />
-            </div>
+              </div>
+            ) : (
+              <div
+                className="flex-1 overflow-y-auto p-4"
+                style={{ minHeight: 0 }} // Ensures flex behavior works properly
+              >
+                {messages.map((message, index) => (
+                  <div
+                    key={index}
+                    className={`flex ${
+                      message.sender === userInfo.id
+                        ? "justify-end"
+                        : "justify-start"
+                    } p-2`}
+                  >
+                    <div
+                      className={`text-white p-3 rounded-lg max-w-[30%] h-auto break-words bg-blue-600`}
+                    >
+                      {message.text}
+                    </div>
+                  </div>
+                ))}
+                <div ref={messagesEndRef} />
+              </div>
+            )}
 
             {/* Input Section */}
             <div className="w-full bg-gray-600 p-4">
@@ -258,7 +284,7 @@ const ChatScreen = () => {
             </div>
           </>
         ) : (
-          <h1 className="text-5xl font-extrabold flex justify-center items-center mt-44 select-none text-gray-600">
+          <h1 className="text-5xl font-extrabold flex justify-center items-center mt-44 ml-10 select-none text-gray-600">
             Select A Chat To Start Conversation
           </h1>
         )}
